e25a383f2e69b18a485b101beccbdbc381f9fb68
khatri-tushar <tushar.khatri.it19@gmail.com>
2020-10-28 11:51:19 +0530

    update

--- DynamicProgramming/1001.cpp
+++ DynamicProgramming/1001.cpp
@@ -164,12 +164,13 @@ int maze(){
 
 
 //====================================================/
+
 int minCostMEM(int sr,int sc,int dr,int dc,vector<vector<int>> &grid,vector<vector<int>> &dp){
 	if(sr==sc&&dr==dc){
 		return dp[sr][sc]=1;
 	}
 	if(dp[sr][sc]!=1e8) return dp[sr][sc];
-	int a=1e8,b=1e8;
+	int a,b;
 	if(sr+1<=dr){
 		a=minCostMEM(sr+1,sc,dr,dc,grid,dp);
 	}
@@ -187,13 +188,65 @@ int minCost(vector<vector<int>> &grid){
 
 	return minCostMEM(0,0,n-1,m-1,grid,dp);	
 }
+//====================================================/
 
+int goldMineMEM(int r,int c, int n, int m, vector<vector<int>>&grid, vector<vector<int>> &dp,vector<vector<int>> &dir){
+	if(c==m-1){
+		return dp[r][c]=grid[r][c];
+	}
+	if(dp[r][c]!=0) return dp[r][c];
+    int maxVal=0;
+	for(int d=0;d<3;d++){
+		int x=r+dir[d][0];
+		int y=c+dir[d][1];
+
+	    if(x>=0&&y>=0&&x<n&&y<m){
+			maxVal=max(maxVal,goldMineMEM(x,y,n,m,grid,dp,dir));
+		}
+	}
+	return dp[r][c]=maxVal+grid[r][c];
+}
 
 //====================================================/
-int staircase(int n){
-	if(n==0){
-		return 1;
+
+int friendsPairingMEM(int n, vector<int> &dp){
+	if(n<=1){
+		return dp[n]=1;
 	}
+	if(dp[n]!=0) return dp[n];
+
+    //faith se single aur double ke answer mangwa lo 
+	int single = friendsPairing(n-1,dp);
+	int pairup=friendsPairing(n-2,dp)*(n-1);
+
+	//jo answer recursion se mila ab usko apna answer bna ke dp mai daal do
+	return dp[n]=single+pairup;
+
+}
+
+int friendsPairingTAB(int N, vector<int> &dp){
+	for(int n=0;n<=n;n++){
+		if(n<=1){
+			return dp[n]=1;
+		}
+		if(dp[n]!=0) return dp[n];
+
+    	 
+		int single = dp[n-1];//friendsPairing(n-1,dp);
+		int pairup=dp[n-2]*(n-1);//friendsPairing(n-2,dp)*(n-1);
+
+		dp[n]=single+pairup;
+	}
+	return dp[N];
+}
+
+//====================================================/
+int staircaseMEM(int n,vector<int> &dp){
+	if(n==0){
+		return dp[n]=1;
+	}
+	if(dp[n]!=0) return dp[n];
+
 	int ans=0;
 	if(n-1>=0){
 		ans+=staircase(n-1);
@@ -204,8 +257,37 @@ int staircase(int n){
 	if(n-3>=0){
 		ans+=staircase(n-3);
 	}
-	return ans;
+	return dp[n]=ans;
 }
+
+//====================================================/
+
+int staircaseMinCostMEM(int n, vector<int> &cost, vector<int> &dp){
+	if(n<=1){
+		return dp[n]=cost[n];
+	}
+	if(dp[n]!=0) return dp[n];
+    /*
+						solve(8)  solve(7) = 5
+						=103  \	   /	
+						      |   |
+						solve(9)  solve(8)
+        =min(5+103)+cost[9] \      /
+ 			5+1=6			 \	  /
+		   					solve(10)
+	                        =6+0 (kuch add hi nahi kr skte ab)
+	*/
+	//faith se 1 ki aur do ki jump ka answer mangwa lo
+	int ekKiJump = staircaseMinCostMEM(n-1,dp);
+	int doKiJump = staircaseMinCostMEM(n-2,dp);
+
+	//ab inh dono mai se min mai cost add krke apna answer bna lo
+
+	
+	return dp[n]=min(ekKiJump,doKiJump) + ((n<cost.size())?cost[n]:0); 
+}
+
+//====================================================/
 
 int dice(int n,vector<int> &dp){
 	if(n<=1){
@@ -222,20 +304,12 @@ int dice(int n,vector<int> &dp){
 	return dp[n]=count;
 }
 
-int diceModified(int n,int d,vector<int> &dp){
-	if(n<=1){
-		return dp[n]=1;
-	}
-	if(dp[n]!=0) return dp[n];
-	int count=0;
 
-	for(int i=1;i<=6;i++){
-		if(n-i>=0&&d-1>0){
-			count+=diceModified(n-i,d-1,dp);
-		}
-	}
-	return dp[n]=count;
-}
+//====================================================/
+
+
+
+
 int main()
 {
 	
@@ -246,6 +320,6 @@ int main()
 	cin>>n>>d;
 
 	vector<int> dp(n+1,0);
-	cout<<diceModified(n,d,dp);
+	cout<<dice(n,dp);
 	return 0;
 }
